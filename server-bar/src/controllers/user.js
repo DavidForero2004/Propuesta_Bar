a6a2b68ee1072172467ec71f55ceb829510340e6
Branditon <brandonescudero2810@gmail.com>
2024-02-16 13:56:27 -0500

    Backend of user

--- server-bar/src/controllers/user.js
+++ server-bar/src/controllers/user.js
@@ -1,7 +1,9 @@
 // user.js
-const bcrypt = require('bcrypt');
-const connection = require('../db/connection.js');
+const bcrypt = require('bcrypt')
+const connection = require('../db/connection.js')
+const jwt = require('jsonwebtoken')
 
+//List User
 const getUser = (_req, res ) =>{
     res.json({
         msg:"Get users"
@@ -15,38 +17,96 @@ const newUser = async (req, res) => {
 
     /////////////////////////////////////////////////////////////////////////////////////
 
-    const userExist = `SELECT COUNT(*) FROM user WHERE email = ${email}`
+    try {
+        const userExist = `SELECT * FROM user WHERE email = '${email}'`
 
-    if (userExist > 0) {
-        res.status(400).json({
-            msg: `There is already a user with this email ${email}`
-        })
-    } else {
-        const query = `INSERT INTO user(name, email, password, id_status, id_rol) VALUES("${name}", "${email}", "${hashPassword}", ${id_status}, ${id_rol})`
-
-        connection.query(query ,(error, result) => {
+        connection.query(userExist, (error, result) => {
             if (error) {
-                res.json({
-                    msg: 'Error to insert',
+                res.status(500).json({
+                    msg: 'Database server error',
                     error
                 })
             } else {
-                res.json({
-                    msg: 'New User',
-                    result
-                })
+                if (result.length > 0) {
+                    res.status(400).json({
+                        msg: `There is already a user with this email ${email}`
+                    })
+                } else {
+                    const query = `INSERT INTO user(name, email, password, id_status, id_rol) VALUES("${name}", "${email}", "${hashPassword}", ${id_status}, ${id_rol})`
+            
+                    connection.query(query ,(error, result) => {
+                        if (error) {
+                            res.json({
+                                msg: 'Error to insert',
+                                error
+                            })
+                        } else {
+                            res.json({
+                                msg: 'New User',
+                                result
+                            })
+                        }
+                    })
+                }
             }
+        })
+    } catch (error) {
+        res.status(502).json({
+            msg: 'Error',
+            error
         })
     }
 };
 
 //Login User
-const loginUser = (req, res) => {
-    const { body } = req;
-    res.json({
-        msg: 'Login User',
-        body
-    });
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    /////////////////////////////////////////////////////////////////////////////////////
+
+    try {
+        const userExist = `SELECT * FROM user WHERE email = '${email}'`
+
+        connection.query(userExist, async (error, result) => {
+            const user = result[0];
+            const passwordvalidate = await bcrypt.compare(password, user.password)
+            const token = jwt.sign({
+                username: email
+            }, process.env.SECRET_K || 'contrasena123', {
+                expiresIn: '10000'
+            })
+
+            /////////////////////////////////////////////////////////////////////////////////////
+            if (error) {
+                res.status(500).json({
+                    msg: 'Database server error',
+                    error
+                })
+            } else {
+                if (!result.length) {
+                    res.status(400).json({
+                        msg: `There is no user with this email ${email}`
+                    })
+                }
+
+                if (!passwordvalidate) {
+                    res.status(400).json({
+                        msg: 'Incorrect password'
+                    })
+                }
+
+                res.json({
+                    token
+                })
+                
+            }
+        })
+    } catch (error) {
+        res.status(502).json({
+            msg: 'Error',
+            error
+        })
+    }
 };
 
 module.exports = { newUser, loginUser, getUser };
--- server-bar/src/routes/user.js
+++ server-bar/src/routes/user.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const { getUser ,newUser, loginUser } = require('../controllers/user');
+const { getUser ,newUser, loginUser } = require('../controllers/user')
+const { validateToken } = require('../routes/validate-token.js')
 
-router.get('/', getUser);
-router.post('/newuser', newUser);
-router.post('/login', loginUser);
+router.get('/', validateToken, getUser)
+router.post('/newuser', newUser)
+router.post('/login', loginUser)
 
 module.exports = router;
 
