f8e22c8e7e5d28718008f72995d341b0afbaaa97
DavidForero2004 <Davidforeromoreno2016@gmail.com>
2024-04-06 18:36:45 -0500

    carousel-landingTry

--- frontend-bar/src/assets/js/landing/landing.js
+++ frontend-bar/src/assets/js/landing/landing.js
@@ -1,51 +1,39 @@
-const menu_hamburguer = document.getElementById('manu_hamb');
-const options_ = document.querySelector('.option_container_menu');
+const menu_hamburguer = document.getElementById("menu_hamb");
+const options_ = document.querySelector(".option_container_menu");
 
 menu_hamburguer.addEventListener('click', () => {
     options_.classList.toggle('show'); // Alternar la clase 'show'
 });
 
 
+const products = document.querySelectorAll(".slider .product_");
+let counter = 0;
 
-
-
-
-
-
-
-
-
-///carousel
-
-/* etiqueta las imágenes pra poder rastrearlas, solo por conveniencia */
-let i = 1;
-for (let li of carousel.querySelectorAll('li')) {
-  li.style.position = 'relative';
-  li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
-  i++;
+function left() {
+    if (counter > 0) {
+        counter--;
+        scroll();
+    }
 }
 
-/* configuración */
-let width = 130; // ancho de las imágenes
-let count = 1; // conteo de las imágenes visibles
 
-let list = carousel.querySelector('ul');
-let listElems = carousel.querySelectorAll('li');
 
-let position = 0; // posición del desplazamiento del carrete
+function right() {
+    if (counter < products.length - 3) {
+        console.log(counter, products.length - 1 )
+        counter++;
+        scroll();
+    } else {
+        // Aquí puedes decidir qué hacer cuando llegas al final de los slides
+        // Por ejemplo, reiniciar el contador a cero
+         counter = 1;
+    }
+}
 
-carousel.querySelector('.prev').onclick = function () {
-  // desplazamiento izquierdo
-  position += width * count;
-  // no podemos mover demasiado a la izquierda, se acaban las imágenes
-  position = Math.min(position, 0)
-  list.style.marginLeft = position + 'px';
-};
+function scroll() {
+    products.forEach(function (item) {
+        item.style.transform = `translateX(${counter * -250}px)`;
+    });
+}
 
-carousel.querySelector('.next').onclick = function () {
-  // desplazamiento derecho
-  position -= width * count;
-  // solo se puede desplazar el carrete de imágenes (longitud total de la cinta - conteo visibles)
-  position = Math.max(position, -width * (listElems.length - count));
-  list.style.marginLeft = position + 'px';
-};
+
