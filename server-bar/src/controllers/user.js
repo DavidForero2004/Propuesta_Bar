e5774198da3a35427a8c168af6f06e762ae872ef
DavidForero2004 <davidforeromoreno2016@gmail.com>
2024-10-13 16:26:20 -0500

    bcrypt arreglo

--- server-bar/src/controllers/user.js
+++ server-bar/src/controllers/user.js
@@ -1,33 +1,23 @@
 //controllers/user.js
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs'); // Cambiar a bcryptjs
 const connection = require('../db/connection.js');
 const jwt = require('jsonwebtoken');
 const i18n = require('i18n');
 
-
-//show all user
+// Show all users
 const getUser = (req, res) => {
     const query = 'CALL selectUser';
 
-    //////////////////////////////////////////////////////////////////////
-
     try {
         connection.query(query, (error, result) => {
-            try {
-                if (error) {
-                    res.status(500).json({
-                        msg: 'Error',
-                        error
-                    });
-                } else {
-                    res.json({
-                        result
-                    });
-                }
-            } catch (error) {
-                res.status(400).json({
+            if (error) {
+                res.status(500).json({
                     msg: 'Error',
                     error
+                });
+            } else {
+                res.json({
+                    result
                 });
             }
         });
@@ -39,62 +29,45 @@ const getUser = (req, res) => {
     }
 }
 
-
-//insert user
+// Insert user
 const insertUser = async (req, res) => {
     const { name, email, password, id_status, id_rol } = req.body;
     const hashPassword = await bcrypt.hash(password, 10);
     const userExist = 'CALL selectUserEmail(?)';
 
-    /////////////////////////////////////////////////////////////////////////////////////
-
     try {
         connection.query(userExist, email, (error, result) => {
-            try {
-                const userData = result[0]; // access the first element of result
-                const user = userData[0]; // the first element of userData contains the RowDataPacket object with the user data
-    
-                if (error) {
-                    res.status(500).json({
-                        msg: i18n.__('errorDatabaseServer'),
-                        error
-                    });
-                } else {
-                    if (user) {
-                        res.status(400).json({
-                            msg: i18n.__('existUserEmail') + ` ${email}`
-                        });
-                        return;
-                    }
-    
-                    const query = 'CALL insertUser(?,?,?,?,?)';
-    
-                    connection.query(query, [name, email, hashPassword, id_status, id_rol], (error, result) => {
-                        try {
-                            if (error) {
-                                res.json({
-                                    msg: i18n.__('errorInsert'),
-                                    error
-                                });
-                                return
-                            } else {
-                                res.json({
-                                    msg: i18n.__('newUser'),
-                                    result
-                                });
-                            }
-                        } catch (error) {
-                            res.status(502).json({
-                                msg: 'Error',
-                                error
-                            });
-                        }
-                    });
-                }
-            } catch (error) {
-                res.status(502).json({
-                    msg: 'Error',
+            const userData = result[0]; // Access the first element of result
+            const user = userData[0]; // The first element of userData contains the RowDataPacket object with the user data
+
+            if (error) {
+                res.status(500).json({
+                    msg: i18n.__('errorDatabaseServer'),
                     error
+                });
+            } else {
+                if (user) {
+                    res.status(400).json({
+                        msg: i18n.__('existUserEmail') + ` ${email}`
+                    });
+                    return;
+                }
+
+                const query = 'CALL insertUser(?,?,?,?,?)';
+
+                connection.query(query, [name, email, hashPassword, id_status, id_rol], (error, result) => {
+                    if (error) {
+                        res.json({
+                            msg: i18n.__('errorInsert'),
+                            error
+                        });
+                        return
+                    } else {
+                        res.json({
+                            msg: i18n.__('newUser'),
+                            result
+                        });
+                    }
                 });
             }
         });
@@ -106,33 +79,23 @@ const insertUser = async (req, res) => {
     }
 }
 
-
-//update user
+// Update user
 const updateUser = async (req, res) => {
     const { id, name, email, password, id_status, id_rol } = req.body;
     const hashPassword = await bcrypt.hash(password, 10);
     const query = 'CALL updateUser(?,?,?,?,?,?)';
 
-    ///////////////////////////////////////////////////////////////////////////
-
     try {
         connection.query(query, [id, name, email, hashPassword, id_status, id_rol], (error, result) => {
-            try {
-                if (error) {
-                    res.status(500).json({
-                        msg: i18n.__('errorUpdate'),
-                        error
-                    });
-                } else {
-                    res.json({
-                        msg: i18n.__('updateUser'),
-                        result
-                    });
-                }
-            } catch (error) {
-                res.status(400).json({
-                    msg: 'Error',
+            if (error) {
+                res.status(500).json({
+                    msg: i18n.__('errorUpdate'),
                     error
+                });
+            } else {
+                res.json({
+                    msg: i18n.__('updateUser'),
+                    result
                 });
             }
         });
@@ -144,32 +107,22 @@ const updateUser = async (req, res) => {
     }
 }
 
-
-//delete user
+// Delete user
 const deleteUser = (req, res) => {
     const { id } = req.params;
     const query = 'CALL deleteUser(?)';
 
-    ///////////////////////////////////////////////////////////
-
     try {
         connection.query(query, id, (error, result) => {
-            try {
-                if (error) {
-                    res.status(500).json({
-                        msg: i18n.__('errorDelete'),
-                        error
-                    });
-                } else {
-                    res.json({
-                        msg: i18n.__('deleteUser'),
-                        result
-                    });
-                }
-            } catch (error) {
-                res.status(400).json({
-                    msg: 'Error',
+            if (error) {
+                res.status(500).json({
+                    msg: i18n.__('errorDelete'),
                     error
+                });
+            } else {
+                res.json({
+                    msg: i18n.__('deleteUser'),
+                    result
                 });
             }
         });
@@ -181,54 +134,44 @@ const deleteUser = (req, res) => {
     }
 }
 
-
-//login user
+// Login user
 const loginUser = (req, res) => {
     const { email, password } = req.body;
     const userExist = 'CALL selectUserEmail(?)';
 
-    /////////////////////////////////////////////////////////////////////////////////////
-
     try {
         connection.query(userExist, [email], async (error, result) => {
-            try {
-                const userData = result[0]; // access the first element of result
-                const user = userData[0]; // the first element of userData contains the RowDataPacket object with the user data
-    
-                if (error) {
-                    res.status(500).json({
-                        msg: i18n.__('errorDatabaseServer'),
-                        error
-                    });
-                } else {
-                    if (!user) {
-                        res.status(400).json({
-                            msg: i18n.__('notExistUserEmail') + ` ${email}`
-                        });
-                        return;
-                    }
-    
-                    const passwordvalidate = await bcrypt.compare(password, user.password);
-    
-                    if (!passwordvalidate) {
-                        res.status(400).json({
-                            msg: i18n.__('incorrectPassword')
-                        });
-                        return;
-                    }
-    
-                    const token = jwt.sign({
-                        username: email
-                    }, process.env.SECRET_K || 'contrasena123');
-    
-                    res.json({
-                        token
-                    });
-                }
-            } catch (error) {
-                res.status(502).json({
-                    msg: 'Error',
+            const userData = result[0]; // Access the first element of result
+            const user = userData[0]; // The first element of userData contains the RowDataPacket object with the user data
+
+            if (error) {
+                res.status(500).json({
+                    msg: i18n.__('errorDatabaseServer'),
                     error
+                });
+            } else {
+                if (!user) {
+                    res.status(400).json({
+                        msg: i18n.__('notExistUserEmail') + ` ${email}`
+                    });
+                    return;
+                }
+
+                const passwordvalidate = await bcrypt.compare(password, user.password);
+
+                if (!passwordvalidate) {
+                    res.status(400).json({
+                        msg: i18n.__('incorrectPassword')
+                    });
+                    return;
+                }
+
+                const token = jwt.sign({
+                    username: email
+                }, process.env.SECRET_K || 'contrasena123');
+
+                res.json({
+                    token
                 });
             }
         });
@@ -240,39 +183,29 @@ const loginUser = (req, res) => {
     }
 }
 
-
-//show user id
+// Show user by id
 const getUserId = (req, res) => {
     const { id } = req.params;
     const query = 'CALL selectUserId(?)';
 
-    ////////////////////////////////////////////////////////////////////
-
     try {
         connection.query(query, id, (error, result) => {
-            try {
-                if (error) {
-                    res.status(400).json({
-                        msg: 'Error',
-                        error
-                    });
-                } else {
-                    if (!result) {
-                        res.status(400).json({
-                            msg: i18n.__('notExistUser'),
-                            result
-                        });
-                    } else {
-                        res.json({
-                            result
-                        });
-                    }
-                }
-            } catch (error) {
-                res.status(500).json({
+            if (error) {
+                res.status(400).json({
                     msg: 'Error',
                     error
                 });
+            } else {
+                if (!result) {
+                    res.status(400).json({
+                        msg: i18n.__('notExistUser'),
+                        result
+                    });
+                } else {
+                    res.json({
+                        result
+                    });
+                }
             }
         });
     } catch (error) {
@@ -282,6 +215,5 @@ const getUserId = (req, res) => {
         });
     }
 }
-
 
 module.exports = { insertUser, loginUser, getUser, deleteUser, updateUser, getUserId };
